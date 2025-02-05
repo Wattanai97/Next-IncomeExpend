9b31702097b3f9d5e93aef1bc708e5d00eea81c7
Wattanai97 <icecodekub@gmail.com>
2025-02-05 17:25:26 +0700

    Update 0.0.3

--- app/components/DropdownComponent.tsx
+++ app/components/DropdownComponent.tsx
@@ -3,15 +3,17 @@ import React from "react";
 import { Dropdown } from "flowbite-react";
 import { useGlobalState } from "../globalstate";
 const DropdownComponent = () => {
-    const settogglereport = useGlobalState().setTogglereport;
+    const {setTogglereport,setToggleform,setToggletrans} = useGlobalState()
     
   return (
     <div className="Trans-Component my-4">
       <Dropdown className="font text-lg" size="lg" label="Open Menu">
-        <Dropdown.Item onClick={()=>{settogglereport((prev:boolean)=>!prev)}}
+        <Dropdown.Item onClick={()=>{setTogglereport((prev:boolean)=>!prev)}}
          className="text-lg font-bold">Report</Dropdown.Item>
-        <Dropdown.Item className="text-lg font-bold">Form</Dropdown.Item>
-        <Dropdown.Item className="text-lg font-bold">
+        <Dropdown.Item onClick={()=>{setToggleform((prev:boolean)=>!prev)}}
+        className="text-lg font-bold">Form</Dropdown.Item>
+        <Dropdown.Item onClick={()=>{setToggletrans((prev:boolean)=>!prev)}}
+        className="text-lg font-bold">
           Tranasctions
         </Dropdown.Item>
       </Dropdown>
